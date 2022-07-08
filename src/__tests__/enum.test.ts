dbfa749e4adcd58bb7d6a34c2a810acf818ea7a4
Stephen <stephen-chew@outlook.com>
2022-07-08 11:15:58 +0800

    Update unit test.

--- src/__tests__/enum.test.ts
+++ src/__tests__/enum.test.ts
@@ -1,10 +1,35 @@
 import { enumFrom, GetEnumType } from '../enum';
 
 describe('enumFor()', () => {
-  it('should', () => {
+  it('should convert given string to string literal', () => {
     const Status = enumFrom('ready', 'running', 'cancelling', 'terminated');
     type Status = GetEnumType<typeof Status>;
 
-    const READY: Status = 'ready';
+    const READY: Status = 'running';
+
+    expect(READY).toBe('running');
+  });
+
+  it('should convert given invalid string to undefined when no fallback is configured', () => {
+    const Status = enumFrom('ready', 'running', 'cancelling', 'terminated');
+    type Status = GetEnumType<typeof Status>;
+
+    const state = Status.of('test');
+
+    expect(state).toBeUndefined();
+  });
+
+  it('should convert given invalid string to undefined when no fallback is configured', () => {
+    const Status = enumFrom(
+      'ready',
+      'running',
+      'cancelling',
+      'terminated'
+    ).fallback('ready');
+    type Status = GetEnumType<typeof Status>;
+
+    const state = Status.of('test');
+
+    expect(state).toBe('ready');
   });
 });
